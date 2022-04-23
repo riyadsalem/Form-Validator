32c661c59ff8374e52bc0844cb4bdcce4c3cbe52
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-23 14:01:13 +0300

    Constraint Validation API

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Code Starts Here..
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/checkValidity
+
+const form = document.querySelector("#form");
+const message = document.querySelector("#message");
+
+let isValid = false;
+function validateForm() {
+  isValid = form.checkValidity();
+  // alert(isValid); // false
+  if (!isValid) {
+    message.textContent = "Please fill out all fields";
+    message.style.color = "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // alert("Yaa");
+  validateForm();
+});
